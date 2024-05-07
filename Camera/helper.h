5440af29ea5e2497f61f1b176c233e188cc5abfa
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-05-07 13:07:45 +0700

    Move Project Into Separate Folder

--- Camera/helper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <jni.h>
-
-#define CHECK(x) if(FAILED(result)){throwRuntime(environment,x);MFShutdown();CoUninitialize();return NULL;}
-
-void throwClass(JNIEnv*, std::string, std::string);
-void throwRuntime(JNIEnv*, std::string);
